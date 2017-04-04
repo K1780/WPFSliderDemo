7d815be059d251342ee670852873e19dc27c3ec6
K1780 <K1780@ICT-421-WS-16>
2017-04-04 17:09:05 +0300

    WFPDemo

--- WPFSliderDemo/MainWindow.xaml.cs
+++ WPFSliderDemo/MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace WPFSliderDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Animal elukka;
         public MainWindow()
         {
             InitializeComponent();
+            //Omat alustukset 
+            elukka = new WPFSliderDemo.Animal();
+            elukka.Name = "Musti";
+            elukka.Happiness = 10;
+            myProgressBar.DataContext = elukka;
+            tbAnimal.DataContext = elukka;
+
+        }
+
+        private void button_click(object sender, RoutedEventArgs e)
+        {
+            // TODO lisätään olio Happiness-ominaistuutta
+            Animal olio = (Animal) myProgressBar.DataContext;
+            olio.Happiness++;
+            
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            //luodaan toinen elukka
+            Animal kisu = new Animal();
+            kisu.Name = "Kitu";
+            kisu.Happiness = 50;
+            myProgressBar.DataContext = kisu;
+            tbAnimal.DataContext = kisu;
         }
     }
 }
